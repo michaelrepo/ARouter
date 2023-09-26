28533a4c27360543f340bb14ea91292d2c3f4565
piyouqing <piyouqing@5i5j.com>
2023-09-26 15:42:39 +0800

    gradle8

--- module-kotlin/src/main/java/com/alibaba/android/arouter/demo/kotlin/KotlinTestActivity.kt
+++ module-kotlin/src/main/java/com/alibaba/android/arouter/demo/kotlin/KotlinTestActivity.kt
@@ -2,25 +2,27 @@ package com.alibaba.android.arouter.demo.kotlin
 
 import android.app.Activity
 import android.os.Bundle
+import android.widget.TextView
 import com.alibaba.android.arouter.facade.annotation.Autowired
 import com.alibaba.android.arouter.facade.annotation.Route
 import com.alibaba.android.arouter.launcher.ARouter
-import kotlinx.android.synthetic.main.activity_kotlin_test.*
 
 @Route(path = "/kotlin/test")
 class KotlinTestActivity : Activity() {
 
     @Autowired
-    @JvmField var name: String? = null
+    @JvmField
+    var name: String? = null
+
     @Autowired
-    @JvmField var age: Int? = 0
+    @JvmField
+    var age: Int? = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         ARouter.getInstance().inject(this)  // Start auto inject.
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_kotlin_test)
-
-        content.text = "name = $name, age = $age"
+        findViewById<TextView>(R.id.content).text = "name = $name, age = $age"
     }
 }
